3d9536bbffe7ceaa0a7c7817d3b994b76323e600
rlindsberg <roderick@karlemstrand.com>
2017-12-13 10:31:28 +0100

    Removed timer3

--- helpers.c
+++ helpers.c
@@ -1,14 +1,5 @@
 #include "helpers.h"
 #include "types.h"
-
-/*
- * Timer3 registers
- */
-#define T3CON       PIC32_R (0x0A00)
-#define T3CONSET    PIC32_R (0x0A08)
-#define TMR3        PIC32_R (0x0A10)
-#define PR3         PIC32_R (0x0A20)
-
 #include <pic32mx.h>
 
 /**
@@ -17,14 +8,6 @@
 void enableButtons() {
     TRISD |= (0x3f << 5);
 }
-
-/**
- * Returns raw button values at port D
- */
-int getButtonInput(void){
-    return (PORTD & (0x7 << 5)) >> 5;
-}
-
 
 /**
  * Enables multi vector mode which allows
@@ -53,25 +36,4 @@ void enableTimer2(int period, int priority, int prescaling, int interrupts) {
     
     // enable
     T2CONSET = 0x8000;
-}
-
-/**
- *  Set up and start timer 3
- */
-void enableTimer3(int period, int priority, int prescaling, int interrupts) {
-    T3CON = 0x0;            // stop timer
-    TMR3 = 0;               // clear timer
-    PR3 = period;           // set period
-    IPCSET(3) = priority;   // set priority
-    
-    IFSCLR(0) = 0x1000;      // reset timer interrupt status flag
-    
-    // enable timer interrupts
-    if (interrupts == 1) { IECSET(0) = 0x1000; }
-    
-    // set prescaling
-    T3CONSET = prescaling << 4;
-    
-    // enable
-    T3CONSET = 0x8000;
 }
